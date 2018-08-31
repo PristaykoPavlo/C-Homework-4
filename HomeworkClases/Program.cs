03227b16785f9e603b8cb4b8a66dab058f7b2525
Pavlo <pristayko.pavlo@gmail.com>
2018-08-31 13:09:10 +0300

    Added new Persons in Main() function

--- HomeworkClases/Program.cs
+++ HomeworkClases/Program.cs
@@ -29,9 +29,10 @@ namespace HomeworkClases
                 name = s;
                 birthYear = b;
             }
-            public int Age()///для чого,якщо э свойство BirthYear?
+            public int Age()
             {
-                return BirthYear;
+                return DateTime.Now.Year-BirthYear;
+                
             }
             public void Input(string s, int b)
             {
@@ -45,7 +46,7 @@ namespace HomeworkClases
             public override string ToString()
             {
                 string str;
-                return str = "Person with name: " + name + " and birth year: " + birthYear;
+                return str = "Person with name: " + name + " and age: " + this.Age()+" years.";
             }
             public void Output()
             {
@@ -64,9 +65,42 @@ namespace HomeworkClases
 
         static void Main(string[] args)
         {
-            Person p1 = new Person("Pavlo", 18);
-            Console.WriteLine(p1.Age());
-            p1.Output();
+            string[] names = { "Bob", "Anna", "Ira", "Alex", "Oleg", "Max" };
+            Random random = new Random();
+
+            Person[] people =new Person[6];
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i] = new Person(names[random.Next(0, 5)], random.Next(1990, 2010));
+                people[i].Output();
+
+            }
+            Console.WriteLine("/////////////////////");
+            for (int i = 1; i < people.Length; i++)
+            {
+                if (people[i] == people[i - 1])
+                {
+                    people[i].Output();
+                    Console.WriteLine("Equals to");
+                    people[i - 1].Output();
+                }
+            }
+            Console.WriteLine("/////////////////////");
+
+            foreach (var item in people)
+            {
+                if (item.Age()<16)
+                {
+                    item.ChangeName("Very Young");
+                }
+                item.Output();
+
+            }
+            Console.WriteLine("/////////////////////");
+
+
+
             Console.ReadLine();
         }
     }
